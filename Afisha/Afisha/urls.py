689a115542b4e0a131f64fbe2f7b8ee0c830560b
KhegaiVladimir <164324259+KhegaiVladimir@users.noreply.github.com>
2024-07-09 01:31:24 +0600

    Homework7

--- Afisha/Afisha/urls.py
+++ Afisha/Afisha/urls.py
@@ -1,18 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from movie_app import views
+from . import swagger
 
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     path('api/v1/movies/', views.movies_list_api_view),
-#     path('api/v1/movies/<int:id>/', views.movie_detail_api_view),
-#     path('api/v1/directors/', views.directors_list_api_view),
-#     path('api/v1/directors/<int:id>/', views.director_detail_api_view),
-#     path('api/v1/reviews/', views.reviews_list_api_view),
-#     path('api/v1/reviews/<int:id>/', views.review_detail_api_view),
-#     path('api/v1/movies/reviews/', views.movies_reviews_list_api_view),
-# ]
-#
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +14,6 @@ urlpatterns = [
     path('api/v1/reviews/<int:id>/', views.ReviewRetrieveUpdateDestroyAPIView.as_view()),
     path('api/v1/movies/reviews/', views.MovieListCreateAPIView.as_view()),
 ]
+
+
+urlpatterns += swagger.urlpatterns
