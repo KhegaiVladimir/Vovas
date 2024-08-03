1f055ff099492a0cc31ccceab57a2fe4fcaac2b9
KhegaiVladimir <164324259+KhegaiVladimir@users.noreply.github.com>
2024-08-03 17:35:50 +0600

    GeeksPro

---
+++ asd/Todo_list/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+from Todo_list.models import Task
+
+admin.site.register(Task)
---
+++ asd/Todo_list/views.py
@@ -0,0 +1,13 @@
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .models import Task
+from .serializers import TaskSerializer
+
+
+class TaskListCreateAPIView(ListCreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+class TaskRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    lookup_field = 'id'
